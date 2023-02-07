0c2dc4383f069f278b57b35908f8e08ead71aab5
Joseph Iannone <joseph.m.iannone@gmail.com>
2023-02-07 00:32:35 -0500

    fix tabs

--- module/Private/Convert-PSCustomObjectToHashtable.ps1
+++ module/Private/Convert-PSCustomObjectToHashtable.ps1
@@ -3,33 +3,34 @@
  * @Author: Joseph Iannone 
  * @Date: 2023-02-07 00:07:33 
  * @Last Modified by: Joseph Iannone
- * @Last Modified time: 2023-02-07 00:17:46
+ * @Last Modified time: 2023-02-07 00:32:19
  */
 #>
 Function Convert-PSCustomObjectToHashtable {
-
-	Param(
-		[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-		[PSCustomObject]$InputObject
-  	)
-
-	[Hashtable]$ht = @{}
-
-	$InputObject.PSObject.properties | Foreach-Object {
-		
-		If ($_.Value.GetType().Name -eq "PSCustomObject") {
-			
-			$ht[$_.Name] = $_.Value | Convert-PSCustomObjectToHashtable
+    
+    Param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [PSCustomObject]$InputObject
+    )
+    
+    [Hashtable]$ht = @{}
+    
+    $InputObject.PSObject.properties | Foreach-Object {
+        
+        If ($_.Value.GetType().Name -eq "PSCustomObject") {
             
-		}
-		
-		Else {
-
-			$ht[$_.Name] = $_.Value
-		}
-
-	}
-
-	$ht
-
+            $ht[$_.Name] = $_.Value | Convert-PSCustomObjectToHashtable
+        
+        }
+        
+        Else {
+            
+            $ht[$_.Name] = $_.Value
+        
+        }
+    
+    }
+    
+    $ht
+    
 }
