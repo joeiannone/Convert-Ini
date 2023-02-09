d09731136c7115bf75e3a55552eb2e2e0244a124
Joseph Iannone <joseph.m.iannone@gmail.com>
2023-02-08 21:56:53 -0500

    additional comments and summary

--- src/IniParser.cs
+++ src/IniParser.cs
@@ -17,50 +17,60 @@ namespace ConvertIni
         /// <returns></returns>
         public static object Parse(string iniString)
         {
+
             string section = string.Empty;
 
             PSObject result = new PSObject();
 
             using (StringReader reader = new StringReader(iniString))
             {
+
+                // regex pattern for section [<section>]
                 Regex iniSectionRgx = new Regex(@"^\[(.+)\]$");
+
+                // regex pattern for entry <key>=<value>
                 Regex iniEntryRgx = new Regex(@"^\s*([^#].+?)\s*=\s*(.*)");
 
                 string line;
 
                 while ((line = reader.ReadLine()) != null)
                 {
+                    // check if there is a match for section or entry in each line
                     var sectionMatch = iniSectionRgx.Match(line);
                     var entryMatch = iniEntryRgx.Match(line);
 
+                    // if new section pattern "[<section>]", set section string
                     if (sectionMatch.Success)
                     {
                         section = sectionMatch.Groups[1].Value.Trim();
                     }
-
+                    // if new entry pattern "<key>=<value>"
                     else if (entryMatch.Success)
                     {
-
+                        // ignore lines commented with ;
                         if (entryMatch.Groups[1].Value.StartsWith(";"))
                         {
                             continue;
                         }
 
+                        // set and trim key and value for entry
                         string iniEntryKey = entryMatch.Groups[1].Value.Trim();
                         string iniEntryValue = entryMatch.Groups[2].Value.Trim();
 
+                        // simply add entry propeties to PSObject when there is no section
                         if (section == string.Empty)
                         {
                             result.Properties.Add(new PSNoteProperty(iniEntryKey, iniEntryValue));
                         }
                         else
                         {
-
+                            // check if section is defined in object and initialize to new PSObject if not
                             if (result.Properties[section] == null)
                             {
                                 result.Properties.Add(new PSNoteProperty(section, new PSObject()));
                             }
 
+                            // add entry to section object
                             ((PSObject)result.Properties[section].Value).Properties.Add(new PSNoteProperty(iniEntryKey, iniEntryValue));
                         
                         }
--- src/IniWriter.cs
+++ src/IniWriter.cs
@@ -7,7 +7,7 @@ namespace ConvertIni
     public class IniWriter
     {
         /// <summary>
-        /// 
+        /// Convert PSObject to ini string
         /// </summary>
         /// <param name="inputObject"></param>
         /// <param name="compressed"></param>
@@ -17,25 +17,32 @@ namespace ConvertIni
 
             string output = string.Empty;
 
+            // initialize an empty list to put entries with no section
             List<string> noSection = new List<string>();
 
+            // determine if extra new lines will be used based on compressed flag
             string room = compressed ? string.Empty : Environment.NewLine;
 
+            // iterate through properties
             foreach (PSNoteProperty item in inputObject.Properties)
             {
+                // make sure item value is set to empty string when null
                 var itemValue = (item.Value == null) ? string.Empty : item.Value;
 
+                // when the current item is an object
                 if (itemValue.GetType() == typeof(PSObject))
                 {
-
+                    // set section
                     output += $"[{item.Name}]{Environment.NewLine}";
 
+                    // recursive call for child object
                     output += Write((PSObject)itemValue, compressed);
 
                 }
 
                 else
                 {
+                    // add entry to no section list to be dealt with later
                     noSection.Add($"{item.Name}={itemValue.ToString()}{Environment.NewLine}");
                 }
 
