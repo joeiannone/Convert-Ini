5cad72d32737502da45169502c49e8e50bfdd83c
Joseph Iannone <joseph.m.iannone@gmail.com>
2023-02-11 11:59:09 -0500

    initialize Remove-IniProperty and Add-IniProperty unit tests

---
+++ module/tests/Add-IniProperty.Test.ps1
@@ -0,0 +1,21 @@
+<#/*
+ * @Author: Joseph Iannone 
+ * @Date: 2023-02-11 11:31:43 
+ * @Last Modified by:   Joseph Iannone 
+ * @Last Modified time: 2023-02-11 11:31:43 
+ */#>
+
+
+BeforeAll {
+
+
+}
+
+
+Describe 'Add-IniProperty' {
+
+    It 'Adds or Updates properties from an input object in a specified ini file' {
+        
+    }
+
+}
---
+++ module/tests/Remove-IniProperty.Test.ps1
@@ -0,0 +1,22 @@
+<#/*
+ * @Author: Joseph Iannone 
+ * @Date: 2023-02-11 11:33:16 
+ * @Last Modified by:   Joseph Iannone 
+ * @Last Modified time: 2023-02-11 11:33:16 
+ */#>
+
+
+BeforeAll {
+
+
+}
+ 
+ 
+Describe 'Remove-IniProperty' {
+
+    It 'Removes a specified property or section from an ini file' {
+
+        
+    }
+
+}
